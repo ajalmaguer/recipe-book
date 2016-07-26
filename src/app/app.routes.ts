9dc8f26fb15bb2ecd64ebc262528fa4f2115ae25
AJ Almaguer <ajalmaguer@AJ-140069.local>
2016-07-26 15:20:14 -0700

    created recipe children routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { provideRouter } from '@angular/router'
 import { RecipesComponent } from './recipes'
 import { ShoppingListComponent } from './shopping-list'
 
+import { RECIPE_ROUTES } from './recipes/recipes.routes'
+
 export const APP_ROUTES_PROVIDERS = [
 	provideRouter([
 		{ path: '', redirectTo: '/recipes', pathMatch: 'full' },
-		{ path: 'recipes', component: RecipesComponent },
+		{ path: 'recipes', component: RecipesComponent , children: RECIPE_ROUTES},
 		{ path: 'shopping-list', component: ShoppingListComponent }
 	])
 ]
